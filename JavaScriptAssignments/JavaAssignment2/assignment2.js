17f79bbdf7fb1f878343bb12fd7f16004f21cbc2
Meg7373 <meaghannlw@gmail.com>
2025-11-14 10:43:40 -0500

    update

--- JavaScriptAssignments/JavaAssignment2/assignment2.js
+++ JavaScriptAssignments/JavaAssignment2/assignment2.js
@@ -1,8 +1,9 @@
 // Javascript Assignment 2
 
-let average = score1 + score2
+function functionAverage (score1, score2) {
 score1 = 80
 score2 = 90
 
 let result = Math.average (average)
 console.log (result)
+}
