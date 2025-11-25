47ce5226ee75132d72f7bbb018ebc0fd8323f84e
Meg7373 <meaghannlw@gmail.com>
2025-11-24 19:08:58 -0500

    update

--- assignment4.js
+++ assignment4.js
@@ -1,10 +1,35 @@
-// Simulate API Response
-
-const articles = ["Title", "Body", "Created Date", "Author Name", "Author Age"];
-const book1 = {
-    "Title": "Learning JSON",
-    "Body": "I am learning to use JSON.",
-    "Created Date": "02-05-2025",
-    "Author Name": "Tim Doe",
-    "Author Age": 29
-}
+// Simulated API Response
+const apiResponse = {
+  "data": [
+    {
+      "id": "1",
+      "title": "Learning JSON",
+      "body": "JSON helps us send and receive data.",
+      "created": "2023-01-10",
+      "author": {
+        "name": "John Doe",
+        "age": 45
+      }
+    },
+    {
+      "id": "2",
+      "title": "Understanding the DOM",
+      "body": "The DOM lets us change the page using JavaScript.",
+      "created": "2023-05-22",
+      "author": {
+        "name": "Mary Smith",
+        "age": 33
+      }
+    },
+    {
+      "id": "3",
+      "title": "JavaScript Essentials",
+      "body": "Loops, variables, functions — all important!",
+      "created": "2024-01-01",
+      "author": {
+        "name": "John Doe",
+        "age": 45
+      }
+    }
+  ]
+};
